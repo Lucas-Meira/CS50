41ee471ef78bd42e0ca3b4fa6bc227811e1dde47
Lucas Meira <lima07.lucas@gmail.com>
2020-11-28 15:45:09 +0100

    bug fixes

--- pset6/readability/readability.py
+++ pset6/readability/readability.py
@@ -9,23 +9,21 @@ L = 0
 
 for word in text:
 
-    L += len(word)
+    L += len(word.strip("\"").rstrip(".|!|?|,"))
 
     if word.find('\'') != -1:
         L -= 1
 
-
-    if word[-1] in ["!", "?", "."]:
+    if any(x in word for x in ["!", "?", "."]):
         S += 1
-        L -= 1
 
 grade = 100 * 0.0588 * L / W - 0.296 * 100 * S / W - 15.8
 
 if (grade < 1):
-    print ("Before Grade 1")
+    print("Before Grade 1")
 
 elif (grade > 16):
-    print ("Grade 16+")
+    print("Grade 16+")
 
 else:
-    print ("Grade " + str(round(grade)))
+    print("Grade " + str(round(grade)))
