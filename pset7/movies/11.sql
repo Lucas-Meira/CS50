5cbd2b2694b3df104dfaeac8c3ab24f5d6f64127
Lucas Meira <lima07.lucas@gmail.com>
2020-12-05 16:21:09 +0100

    error correction

--- pset7/movies/11.sql
+++ pset7/movies/11.sql
@@ -1,1 +1,8 @@
-SELECT title FROM movies WHERE id IN (SELECT stars.movie_id FROM stars JOIN ratings ON stars.movie_id = ratings.movie_id WHERE person_id IN(SELECT id FROM people WHERE name IS "Chadwick Boseman") ORDER BY rating DESC) LIMIT 5;
+SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id
+WHERE id IN
+(SELECT movie_id FROM stars
+WHERE person_id IN
+(SELECT id FROM people
+WHERE name IS "Chadwick Boseman"))
+ORDER BY rating DESC
+LIMIT 5;
